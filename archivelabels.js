37647a4e079aac6ac9dd6f9dea9141259ef32384
etassone <etassone@users.noreply.github.com>
2017-12-15 08:55:59 -0700

    Create archivelabels.js

---
+++ archivelabels.js
@@ -0,0 +1,14 @@
+##auto archive labeled emails from gmail
+##from hcon94 
+##run in scripts.google.com
+function gmailAutoarchive() {
+  // Get all the threads that are labelled
+  var labels = GmailApp.getUserLabels();
+  for (var a = 0; a < labels.length; a++) {
+    var threads = labels[a].getThreads(0, 500);
+    // Archive all the labelled threads
+    for (var i = 0; i < threads.length; i++) {
+      threads[i].moveToArchive();
+    }
+  }
+}
